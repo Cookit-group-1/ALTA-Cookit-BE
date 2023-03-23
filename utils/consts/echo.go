99a56f00c839a136dba490720b75a8f6f0146f47
Andreas <Andreas.y45z@gmail.com>
2023-03-23 17:29:47 +0700

    Add path parameter consts to echo

--- utils/consts/echo.go
+++ utils/consts/echo.go
@@ -1,6 +1,16 @@
 package consts
 
 const (
+	ECHO_P_FollowerId = "follower_id"
+	ECHO_P_RecipeId = "recipe_id"
+	ECHO_P_LikeId = "like_id"
+	ECHO_P_CommentId = "comment_id"
+	ECHO_P_IngredientId = "ingredient_id"
+	ECHO_P_IngredientDetailId = "ingredient_detail_id"
+	ECHO_P_CartId = "cart_id"
+)
+
+const (
 	ECHO_InvaildIdParam    string = "invaild id param"
 	ECHO_InvaildPageParam  string = "invalid page param"
 	ECHO_InvaildLimitParam string = "invalid limit param"
