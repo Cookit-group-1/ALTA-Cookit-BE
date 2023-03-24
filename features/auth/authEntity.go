ed22487494568618e0f8894573b9eb44e91e86fa
alfianadityads <alfianadityadwi@gmail.com>
2023-03-24 16:28:34 +0700

    build core struct

--- features/auth/authEntity.go
+++ features/auth/authEntity.go
@@ -1,2 +1,12 @@
 package auth
 
+import "github.com/labstack/echo/v4"
+
+type Core struct {
+	ID             uint
+	ProfilePicture string
+	Username       string `validate:"required"`
+	Email          string `validate:"required,email"`
+	Password       string `validate:"required,min=3"`
+}
+
