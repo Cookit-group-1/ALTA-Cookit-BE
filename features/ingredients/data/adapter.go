5914da9714dfa1f9c62bf20fa4d95ff075192693
Andreas <Andreas.y45z@gmail.com>
2023-03-24 20:59:32 +0700

    Update ingredients features - Adapters

--- features/ingredients/data/adapter.go
+++ features/ingredients/data/adapter.go
@@ -3,6 +3,7 @@ package data
 import (
 	"alta-cookit-be/features/ingredients"
 	_ingredientModel "alta-cookit-be/features/ingredients/models"
+	_ingredientDetailData "alta-cookit-be/features/ingredient_details/data"
 )
 
 func ConvertToGorm(entity *ingredients.IngredientEntity) *_ingredientModel.Ingredient {
@@ -10,11 +11,20 @@ func ConvertToGorm(entity *ingredients.IngredientEntity) *_ingredientModel.Ingre
 		RecipeID: entity.RecipeID,
 		Name:     entity.Name,
 		Price:    entity.Price,
+		IngredientDetails: *_ingredientDetailData.ConvertToGorms(&entity.IngredientDetailEntities),
 	}
 	if entity.ID != 0 {
 		gorm.ID = entity.ID
 	}
 	return &gorm
+}
+
+func ConvertToGorms(entities *[]ingredients.IngredientEntity) *[]_ingredientModel.Ingredient {
+	gorms := []_ingredientModel.Ingredient{}
+	for _, entity := range *entities {
+		gorms = append(gorms, *ConvertToGorm(&entity))
+	}
+	return &gorms
 }
 
 func ConvertToEntity(gorm *_ingredientModel.Ingredient) *ingredients.IngredientEntity {
@@ -23,5 +33,14 @@ func ConvertToEntity(gorm *_ingredientModel.Ingredient) *ingredients.IngredientE
 		RecipeID: gorm.RecipeID,
 		Name: gorm.Name,
 		Price: gorm.Price,
+		IngredientDetailEntities: *_ingredientDetailData.ConvertToEntities(&gorm.IngredientDetails),
 	}
+}
+
+func ConvertToEntities(gorms *[]_ingredientModel.Ingredient) *[]ingredients.IngredientEntity {
+	entities := []ingredients.IngredientEntity{}
+	for _, gorm := range *gorms {
+		entities = append(entities, *ConvertToEntity(&gorm))
+	}
+	return &entities
 }
--- features/ingredients/delivery/adapter.go
+++ features/ingredients/delivery/adapter.go
@@ -1,6 +1,9 @@
 package delivery
 
-import "alta-cookit-be/features/ingredients"
+import (
+	"alta-cookit-be/features/ingredients"
+	_ingredientDetailDelivery "alta-cookit-be/features/ingredient_details/delivery"
+)
 
 func ConvertToEntity (request *ingredients.IngredientRequest) *ingredients.IngredientEntity {
 	return &ingredients.IngredientEntity{
@@ -10,7 +13,16 @@ func ConvertToEntity (request *ingredients.IngredientRequest) *ingredients.Ingre
 		RecipeID: request.RecipeID,
 		Name:     request.Name,
 		Price:    request.Price,
+		IngredientDetailEntities: *_ingredientDetailDelivery.ConvertToEntities(&request.IngredientDetailRequests),
 	}
+}
+
+func ConvertToEntities (requests *[]ingredients.IngredientRequest) *[]ingredients.IngredientEntity {
+	entities := []ingredients.IngredientEntity{}
+	for _, request := range *requests {
+		entities = append(entities, *ConvertToEntity(&request))
+	}
+	return &entities
 }
 
 func ConvertToResponse (entity *ingredients.IngredientEntity) ingredients.IngredientResponse {
@@ -18,5 +30,14 @@ func ConvertToResponse (entity *ingredients.IngredientEntity) ingredients.Ingred
 		ID:    entity.ID,
 		Name:  entity.Name,
 		Price: entity.Price,
+		IngredientDetailResponses: _ingredientDetailDelivery.ConvertToResponses(&entity.IngredientDetailEntities),
 	}
+}
+
+func ConvertToResponses (entities *[]ingredients.IngredientEntity) []ingredients.IngredientResponse {
+	responses := []ingredients.IngredientResponse{}
+	for _, entity := range *entities {
+		responses = append(responses, ConvertToResponse(&entity))
+	}
+	return responses
 }
