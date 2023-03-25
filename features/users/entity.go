7317ff6208337e41f22afb0cec84d0ec74ad030b
alfianadityads <alfianadityadwi@gmail.com>
2023-03-26 00:42:46 +0700

    chore: make method for update, show and deactive user

--- features/users/entity.go
+++ features/users/entity.go
@@ -1,6 +1,8 @@
 package users
 
 import (
+	"mime/multipart"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -17,15 +19,24 @@ type Core struct {
 type UserHandler interface {
 	Register() echo.HandlerFunc
 	Login() echo.HandlerFunc
+	Profile() echo.HandlerFunc
+	Update() echo.HandlerFunc
+	Deactive() echo.HandlerFunc
 }
 
 type UserService interface {
 	Register(newUser Core) (Core, error)
 	Login(username, password string) (string, Core, error)
+	Profile(token interface{}) (Core, error)
+	Update(token interface{}, fileData multipart.FileHeader, updateData Core) (Core, error)
+	Deactive(token interface{}) error
 }
 
 type UserData interface {
 	SelectUserById(existUser Core) *Core
 	Register(newUser Core) (Core, error)
 	Login(username string) (Core, error)
+	Profile(userID uint) (Core, error)
+	Update(userID uint, updateData Core) (Core, error)
+	Deactive(userID uint) error
 }
