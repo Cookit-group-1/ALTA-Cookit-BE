96cb76dc77e734c19f8fe27ff8853a16ce6fa0a1
Andreas <Andreas.y45z@gmail.com>
2023-03-25 08:01:39 +0700

    Update recipes features

--- features/recipes/delivery/recipe.go
+++ features/recipes/delivery/recipe.go
@@ -2,6 +2,7 @@ package delivery
 
 import (
 	"alta-cookit-be/features/recipes"
+	"alta-cookit-be/middlewares"
 	"alta-cookit-be/utils/consts"
 	"alta-cookit-be/utils/helpers"
 	"errors"
@@ -21,6 +22,7 @@ func New(recipeService recipes.RecipeService_) recipes.RecipeDelivery_ {
 }
 
 func (d *RecipeDelivery) InsertRecipe(e echo.Context) error {
+	userId, _, _ := middlewares.ExtractToken(e)
 	recipeRequest := recipes.RecipeRequest{}
 	err := e.Bind(&recipeRequest)
 	if err != nil {
@@ -32,6 +34,7 @@ func (d *RecipeDelivery) InsertRecipe(e echo.Context) error {
 		recipeRequest.Image = append(recipeRequest.Image, file)
 		recipeRequest.ImageName = append(recipeRequest.ImageName, fileNames[index])
 	}
+	recipeRequest.UserID = userId
 
 	output, err := d.recipeService.InsertRecipe(ConvertToEntity(&recipeRequest))
 	if err != nil {
@@ -41,6 +44,7 @@ func (d *RecipeDelivery) InsertRecipe(e echo.Context) error {
 }
 
 func (d *RecipeDelivery) UpdateRecipeById(e echo.Context) error {
+	userId, _, _ := middlewares.ExtractToken(e)
 	recipeId, err := helpers.ExtractIDParam(e, consts.ECHO_P_RecipeId)
 	if err != nil {
 		return errors.New(consts.ECHO_InvaildIdParam)
@@ -52,6 +56,7 @@ func (d *RecipeDelivery) UpdateRecipeById(e echo.Context) error {
 		return helpers.ReturnBadResponse(e, err)
 	}
 	recipeRequest.ID = recipeId
+	recipeRequest.UserID = userId
 
 	err = d.recipeService.UpdateRecipeById(ConvertToEntity(&recipeRequest))
 	if err != nil {
@@ -61,6 +66,7 @@ func (d *RecipeDelivery) UpdateRecipeById(e echo.Context) error {
 }
 
 func (d *RecipeDelivery) DeleteRecipeById(e echo.Context) error {
+	userId, _, _ := middlewares.ExtractToken(e)
 	recipeId, err := helpers.ExtractIDParam(e, consts.ECHO_P_RecipeId)
 	if err != nil {
 		return errors.New(consts.ECHO_InvaildIdParam)
@@ -72,6 +78,7 @@ func (d *RecipeDelivery) DeleteRecipeById(e echo.Context) error {
 		return helpers.ReturnBadResponse(e, err)
 	}
 	recipeRequest.ID = recipeId
+	recipeRequest.UserID = userId
 
 	err = d.recipeService.DeleteRecipeById(ConvertToEntity(&recipeRequest))
 	if err != nil {
