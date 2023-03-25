fe725b5314c593e9b448e0f6f91c20f776afcc9a
alfianadityads <alfianadityadwi@gmail.com>
2023-03-26 01:48:23 +0700

    chore: add config aws s3 and add secret key script

--- app/config/config.go
+++ app/config/config.go
@@ -12,9 +12,9 @@ var (
 	SECRET_JWT        string = ""
 	GCP_PROJECT_ID    string = ""
 	GCP_BUCKET_NAME   string = ""
-	// AWS_REGION               = ""
-	// ACCESS_KEY_ID            = ""
-	// ACCESS_KEY_SECRET        = ""
+	AWS_REGION               = ""
+	ACCESS_KEY_ID            = ""
+	ACCESS_KEY_SECRET        = ""
 	// MIDTRANS_SERVER_KEY string = ""
 )
 
@@ -27,9 +27,9 @@ type AppConfig struct {
 	JWT_KEY           string
 	GCP_PROJECT_ID    string
 	GCP_BUCKET_NAME   string
-	// AWS_REGION        string
-	// ACCESS_KEY_ID     string
-	// ACCESS_KEY_SECRET string
+	AWS_REGION        string
+	ACCESS_KEY_ID     string
+	ACCESS_KEY_SECRET string
 	// MIDTRANS_SERVER_KEY string
 }
 
@@ -80,19 +80,19 @@ func ReadEnv() *AppConfig {
 	// }
 
 	// looking image env for aws s3 bucket
-	// if val, found := os.LookupEnv("AWS_REGION"); found {
-	// 	app.AWS_REGION = val
-	// 	isRead = false
-	// }
-	// if val, found := os.LookupEnv("ACCESS_KEY_ID"); found {
-	// 	// cnv, _ := strconv.Atoi(val)
-	// 	app.ACCESS_KEY_ID = val
-	// 	isRead = false
-	// }
-	// if val, found := os.LookupEnv("ACCESS_KEY_SECRET"); found {
-	// 	app.ACCESS_KEY_SECRET = val
-	// 	isRead = false
-	// }
+	if val, found := os.LookupEnv("AWS_REGION"); found {
+		app.AWS_REGION = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("ACCESS_KEY_ID"); found {
+		// cnv, _ := strconv.Atoi(val)
+		app.ACCESS_KEY_ID = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("ACCESS_KEY_SECRET"); found {
+		app.ACCESS_KEY_SECRET = val
+		isRead = false
+	}
 
 	if isRead {
 		viper.AddConfigPath(".")
@@ -112,9 +112,9 @@ func ReadEnv() *AppConfig {
 		app.DB_NAME = viper.Get("DB_NAME").(string)
 		app.GCP_PROJECT_ID = (viper.Get("GCP_PROJECT_ID").(string))
 		app.GCP_BUCKET_NAME = viper.Get("GCP_BUCKET_NAME").(string)
-		// app.AWS_REGION = viper.Get("AWS_REGION").(string)
-		// app.ACCESS_KEY_ID = viper.Get("ACCESS_KEY_ID").(string)
-		// app.ACCESS_KEY_SECRET = viper.Get("ACCESS_KEY_SECRET").(string)
+		app.AWS_REGION = viper.Get("AWS_REGION").(string)
+		app.ACCESS_KEY_ID = viper.Get("ACCESS_KEY_ID").(string)
+		app.ACCESS_KEY_SECRET = viper.Get("ACCESS_KEY_SECRET").(string)
 		// app.MIDTRANS_SERVER_KEY = viper.Get("MIDTRANS_SERVER_KEY").(string)
 
 	}
@@ -122,9 +122,9 @@ func ReadEnv() *AppConfig {
 	SECRET_JWT = app.JWT_KEY
 	GCP_PROJECT_ID = app.GCP_PROJECT_ID
 	GCP_BUCKET_NAME = app.GCP_BUCKET_NAME
-	// AWS_REGION = app.AWS_REGION
-	// ACCESS_KEY_ID = app.ACCESS_KEY_ID
-	// ACCESS_KEY_SECRET = app.ACCESS_KEY_SECRET
+	AWS_REGION = app.AWS_REGION
+	ACCESS_KEY_ID = app.ACCESS_KEY_ID
+	ACCESS_KEY_SECRET = app.ACCESS_KEY_SECRET
 	// MIDTRANS_SERVER_KEY = app.MIDTRANS_SERVER_KEY
 	return &app
 }
