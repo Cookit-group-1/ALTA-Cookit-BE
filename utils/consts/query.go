aece46d311007cd9c05fb6eaf1fd320394fbf72b
alfianadityads <alfianadityadwi@gmail.com>
2023-03-26 15:50:35 +0700

    chore: build response for user

--- utils/consts/query.go
+++ utils/consts/query.go
@@ -6,4 +6,7 @@ const (
 	QUERY_NotFound        string = "data not found"
 	QUERY_ErrorInsertData string = "error insert data"
 	QUERY_NoRowsAffected  string = "row affected = 0"
+	QUERY_ErrorUpdateData string = "error update data"
+	QUERY_ErrorDeleteData string = "error delete data"
+	QUERY_ErrorReadData   string = "error read data"
 )
