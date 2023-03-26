737875c7abde2294b9fc45b703d11fc90f1f1aa1
Andreas <Andreas.y45z@gmail.com>
2023-03-26 19:44:20 +0700

    Update recipe consts

--- utils/consts/recipe.go
+++ utils/consts/recipe.go
@@ -11,7 +11,7 @@ const (
 	RECIPE_SuccessReadListOfRecipes string = "succesfully read list of recipes"
 	RECIPE_SuccessReadListOfUserRecipes string = "succesfully read list of user's recipes"
 	RECIPE_SuccessInsertUserRecipe string = "succesfully insert user's recipe"
-	RECIPE_SuccessReadDetailsIfRecipe string = "succesfully read details of recipe"
+	RECIPE_SuccessReadDetailsOfRecipe string = "succesfully read details of recipe"
 	RECIPE_SuccessUpdateUserRecipe string = "succesfully updating user's recipe"
 	RECIPE_SucessDeleteUserRecipe string = "succesfully deleting user's recipe"
 )
