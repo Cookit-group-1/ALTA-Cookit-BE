646ef1908746979f9d7d31f1aa525443071386a3
alfianadityads <alfianadityadwi@gmail.com>
2023-03-27 01:55:14 +0700

    chore: add method for search fiture

--- features/users/entity.go
+++ features/users/entity.go
@@ -27,6 +27,7 @@ type UserHandler interface {
 	Deactive() echo.HandlerFunc
 	UpdatePassword() echo.HandlerFunc
 	UpgradeUser() echo.HandlerFunc
+	Search() echo.HandlerFunc
 }
 
 type UserService interface {
@@ -37,6 +38,7 @@ type UserService interface {
 	Deactive(userID uint) error
 	UpdatePassword(userID uint, updatePassword Core) error
 	UpgradeUser(userID uint, approvement Core) (Core, error)
+	Search(quote string) ([]Core, error)
 }
 
 type UserData interface {
@@ -47,4 +49,5 @@ type UserData interface {
 	Update(userID uint, updateData Core) (Core, error)
 	Deactive(userID uint) error
 	UpgradeUser(userID uint, approvement Core) (Core, error)
+	Search(quote string) ([]Core, error)
 }
