5e30b226b6e03230f91925583fae26e163d9b39d
Andreas <Andreas.y45z@gmail.com>
2023-03-27 05:11:20 +0700

    Update comments features - Fix Bug

--- features/comments/models/comment.go
+++ features/comments/models/comment.go
@@ -8,6 +8,6 @@ type Comment struct {
 	gorm.Model
 	UserID   uint
 	RecipeID uint
-	Comment  string `gorm:"not null;type:text"`
-	UrlImage string `gorm:"not null;type:text;d`
+	Comment  string `gorm:"default:'';not null;type:text"`
+	UrlImage string `gorm:"default:'';not null;type:text;"`
 }
