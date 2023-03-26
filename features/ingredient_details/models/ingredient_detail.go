2348102d2597e4a40e24ba3142431d3ac7dded75
Andreas <Andreas.y45z@gmail.com>
2023-03-27 05:13:50 +0700

    Update ingrediet details features - Fix Bug

--- features/ingredient_details/models/ingredient_detail.go
+++ features/ingredient_details/models/ingredient_detail.go
@@ -7,7 +7,7 @@ import (
 type IngredientDetail struct {
 	gorm.Model
 	IngredientID uint
-	Name         string `gorm:"not null;type:VARCHAR(50)"`
+	Name         string `gorm:"default:'';not null;type:VARCHAR(50)"`
 	Quantity     int    `gorm:"not null;default:1"`
-	Unit         string `gorm:"not null;type:VARCHAR(50)"`
+	Unit         string `gorm:"default:'';not null;type:VARCHAR(50)"`
 }
