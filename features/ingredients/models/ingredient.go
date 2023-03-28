47d2aa38d142a325462e1f4de239a2e1bad39788
Andreas <Andreas.y45z@gmail.com>
2023-03-29 06:58:12 +0700

    Update ingredient models

--- features/ingredients/models/ingredient.go
+++ features/ingredients/models/ingredient.go
@@ -11,7 +11,7 @@ import (
 type Ingredient struct {
 	gorm.Model
 	RecipeID          uint
-	Name              string                                    `gorm:"default:'';not null;type:VARCHAR(1000)"`
+	Name              string                                    `gorm:"default:'';not null;type:VARCHAR(255)"`
 	Price             float64                                   `gorm:"not null;default:0"`
 	IngredientDetails []_ingredientDetailModel.IngredientDetail `gorm:"constraint:OnDelete:CASCADE;"`
 	Carts             []_cartModel.Cart                         `gorm:"constraint:OnDelete:CASCADE;"`
