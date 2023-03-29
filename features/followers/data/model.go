4a213561af1a1532f3ce0dc4332d83b4878c8f76
alfianadityads <alfianadityadwi@gmail.com>
2023-03-29 15:57:40 +0700

    chore: make follower struct model and all function

--- features/followers/data/model.go
+++ features/followers/data/model.go
@@ -1,9 +1,35 @@
 package data
 
-import "gorm.io/gorm"
+import (
+	"alta-cookit-be/features/followers"
+
+	"gorm.io/gorm"
+)
 
 type Follower struct {
 	gorm.Model
-	FromUserID uint `gorm:"uniqueIndex:idx_user"`
-	ToUserID   uint `gorm:"uniqueIndex:idx_user"`
+	FromUserID      uint `gorm:"uniqueIndex:idx_user"`
+	ToUserID        uint `gorm:"uniqueIndex:idx_user"`
+	AmountFollowing uint
+	AmountFollowers uint
+}
+
+func DataToCore(data Follower) followers.FollowCore {
+	return followers.FollowCore{
+		ID:                  data.ID,
+		FollowersFromUserID: data.FromUserID,
+		FollowingToUserID:   data.ToUserID,
+		AmountFollowing:     data.AmountFollowing,
+		AmountFollowers:     data.AmountFollowers,
+	}
+}
+
+func CoreToData(data followers.FollowCore) Follower {
+	return Follower{
+		Model:           gorm.Model{ID: data.ID},
+		FromUserID:      data.FollowersFromUserID,
+		ToUserID:        data.FollowingToUserID,
+		AmountFollowing: data.AmountFollowing,
+		AmountFollowers: data.AmountFollowers,
+	}
 }
