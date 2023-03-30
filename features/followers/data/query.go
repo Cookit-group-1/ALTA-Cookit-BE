d7ea875fce97950f7ba018364ee609b24cf2ae57
alfianadityads <alfianadityadwi@gmail.com>
2023-03-30 22:11:29 +0700

    feat: query show follower

--- features/followers/data/query.go
+++ features/followers/data/query.go
@@ -42,7 +42,23 @@ func (fq *FollowQuery) Follow(userID, followingID uint) error {
 
 // ShowAllFollower implements followers.FollowData
 func (fq *FollowQuery) ShowAllFollower(userID uint) ([]followers.FollowCore, error) {
-	panic("unimplemented")
+	follower := []Follower{}
+	log.Println(follower)
+	err := fq.db.Raw("SELECT u.id, u.username, u.profile_picture, u.role, f.to_user_id FROM users u JOIN followers f ON u.id = f.from_user_id WHERE u.id = ?", userID).Scan(&follower).Error
+	if err != nil {
+		log.Println("no data processed", err.Error())
+		return []followers.FollowCore{}, errors.New("no following data found")
+	}
+
+	res := []followers.FollowCore{}
+	for i := 0; i < len(follower); i++ {
+		res = append(res, DataToCore(follower[i]))
+
+	}
+	if len(res) == 0 {
+		return []followers.FollowCore{}, errors.New("no following data found")
+	}
+	return res, nil
 }
 
 // ShowAllFollowing implements followers.FollowData
