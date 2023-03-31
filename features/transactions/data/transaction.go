ac5f942d74656449cfdb560024fab41ce12a5003
Andreas <Andreas.y45z@gmail.com>
2023-03-31 09:04:47 +0700

    Update transaction features - Select order by created_at desc

--- features/transactions/data/transaction.go
+++ features/transactions/data/transaction.go
@@ -68,7 +68,7 @@ func (d *TransactionData) SelectTransactionsByUserId(entity *transactions.Transa
 		}
 	}
 
-	tx := d.db.Debug().Preload("TransactionDetails").Where("user_id = ?", entity.CustomerUserId).Where(qString).Limit(entity.DataLimit).Offset(entity.DataOffset).Find(&gorms)
+	tx := d.db.Debug().Preload("TransactionDetails").Where("user_id = ?", entity.CustomerUserId).Where(qString).Limit(entity.DataLimit).Offset(entity.DataOffset).Order("created_at desc").Find(&gorms)
 	if tx.Error != nil {
 		return nil, tx.Error
 	}
