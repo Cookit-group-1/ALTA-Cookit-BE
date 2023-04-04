283a60ad3754d3d086f1f1de59df3883b6e4c068
alfianadityads <alfianadityadwi@gmail.com>
2023-04-04 21:54:07 +0700

    test: show all following

--- features/followers/services/service_test.go
+++ features/followers/services/service_test.go
@@ -1,8 +1,13 @@
 package services
 
 import (
+	"alta-cookit-be/features/followers"
 	"alta-cookit-be/mocks"
 	"errors"
+	"log"
+	"mime/multipart"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -57,3 +62,103 @@ func TestUnfollow(t *testing.T) {
 	})
 
 }
+
+func TestShowAllFollowing(t *testing.T) {
+	data := mocks.NewFollowData(t)
+	filePath := filepath.Join("..", "..", "..", "test.jpg")
+	imageTrue, err := os.Open(filePath)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	imageTrueCnv := &multipart.FileHeader{
+		Filename: imageTrue.Name(),
+	}
+
+	resData := []followers.FollowCore{{
+		ID:             1,
+		Username:       "abu",
+		Role:           "VerifiedUser",
+		ProfilePicture: imageTrueCnv.Filename,
+		FromUserID:     9,
+		ToUserID:       1,
+	}}
+
+	t.Run("success get all following", func(t *testing.T) {
+		data.On("ShowAllFollowing", uint(1)).Return(resData, nil).Once()
+		srv := New(data)
+		res, err := srv.ShowAllFollowing(uint(1))
+		assert.Nil(t, err)
+		assert.Equal(t, len(resData), len(res))
+		data.AssertExpectations(t)
+	})
+
+	t.Run("internal server error", func(t *testing.T) {
+		data.On("ShowAllFollowing", uint(1)).Return([]followers.FollowCore{}, errors.New("internal server error")).Once()
+		srv := New(data)
+		res, err := srv.ShowAllFollowing(uint(1))
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "server error")
+		assert.Equal(t, 0, len(res))
+		data.AssertExpectations(t)
+	})
+
+	t.Run("user not found", func(t *testing.T) {
+		data.On("ShowAllFollowing", uint(1)).Return([]followers.FollowCore{}, errors.New("user not found")).Once()
+		srv := New(data)
+		res, err := srv.ShowAllFollowing(uint(1))
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "not found")
+		assert.Equal(t, 0, len(res))
+		data.AssertExpectations(t)
+	})
+}
+
+// func TestShowAllFollower(t *testing.T) {
+// 	data := mocks.NewFollowData(t)
+// 	filePath := filepath.Join("..", "..", "..", "test.jpg")
+// 	imageTrue, err := os.Open(filePath)
+// 	if err != nil {
+// 		log.Println(err.Error())
+// 	}
+// 	imageTrueCnv := &multipart.FileHeader{
+// 		Filename: imageTrue.Name(),
+// 	}
+
+// 	resData := []followers.FollowCore{{
+// 		ID:             1,
+// 		Username:       "abu",
+// 		Role:           "VerifiedUser",
+// 		ProfilePicture: imageTrueCnv.Filename,
+// 		FromUserID:     9,
+// 		ToUserID:       1,
+// 	}}
+
+// 	t.Run("success get all follower", func(t *testing.T) {
+// 		data.On("ShowAllFollower", uint(1)).Return(resData, nil).Once()
+// 		srv := New(data)
+// 		res, err := srv.ShowAllFollower(uint(1))
+// 		assert.Nil(t, err)
+// 		assert.Equal(t, len(resData), len(res))
+// 		data.AssertExpectations(t)
+// 	})
+
+// 	t.Run("internal server error", func(t *testing.T) {
+// 		data.On("ShowAllFollower", uint(1)).Return([]followers.FollowCore{}, errors.New("internal server error")).Once()
+// 		srv := New(data)
+// 		res, err := srv.ShowAllFollower(uint(1))
+// 		assert.NotNil(t, err)
+// 		assert.ErrorContains(t, err, "server error")
+// 		assert.Equal(t, 0, len(res))
+// 		data.AssertExpectations(t)
+// 	})
+
+// 	t.Run("user not found", func(t *testing.T) {
+// 		data.On("ShowAllFollower", uint(1)).Return([]followers.FollowCore{}, errors.New("user not found")).Once()
+// 		srv := New(data)
+// 		res, err := srv.ShowAllFollower(uint(1))
+// 		assert.NotNil(t, err)
+// 		assert.ErrorContains(t, err, "not found")
+// 		assert.Equal(t, 0, len(res))
+// 		data.AssertExpectations(t)
+// 	})
+// }
