90f65643bec57e43fddd99912b15d532105ff8a7
Andreas <Andreas.y45z@gmail.com>
2023-04-12 13:24:04 +0700

    Use static conf - Main.go

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
 	}))
+	e.Static("/static", "static")
 	router.InitRouter(db, e)
 
 	e.Logger.Fatal(e.Start(":8083"))
